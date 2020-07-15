20045716826d0fad62edbdc95f0580a8dc886c2e
weonsj <weonsj@gmail.com>
2020-07-15 20:14:13 +0900

    office commit(20200715)

--- main.c
+++ main.c
@@ -285,22 +285,257 @@
 //    }
 //}
 
+//int main() {
+//    float ave_score;
+//    float math, english, science, programming;
+//
+//    printf("수학, 영어, 과학, 컴퓨터 프로그래밍 점수를 각각 입력해 주세요! ");
+//    scanf("%f %f %f %f", &math, &english, &science, &programming);
+//
+//    ave_score =
+//            (math + english + science + programming) / 4;
+//    printf("당신의 평균 점수는 %f입니다. \n", ave_score);
+//    if (ave_score >= 90) {
+//        printf("당신은 우등생입니다. ");
+//    } else if (ave_score >= 30) {
+//        printf("조금만 노력하세요!. \n");
+//    } else  if (ave_score > 0) {
+//        printf("공부를 발로 합니까? \n");
+//    } else {
+//        printf("인생을 포기하셨군요.");
+//    }
+//    return 0;
+//}
+
+//int main() {
+//    int a;
+//    printf("아무 숫자나 입력하세요 : ");
+//    scanf("%d", &a);
+//
+//    if (a >= 10 & a < 20) {
+//            printf("%d는 10이상, 20미만인 수 입니다. \n", a);
+//    }
+//    return 0;
+//}
+
+//int main() {
+//    int height, weight;
+//    printf("당신의 키와 몸무게를 각각 입력해 주세요 : ");
+//    scanf("%d %d", &height, &weight);
+//
+//    if (height >= 190 || weight >= 100) {
+//        printf("당신은 '거구'입니다. \n");
+//    } else {
+//        printf("당신은 '거구'가 아닙니다. \n");
+//    }
+//    return 0;
+//}
+
+//int main() {
+//    int i;
+//    for (i = 0; i < 20; i++) {
+//        printf("숫자 : %d \n", i);
+//    }
+//    return 0;
+//}
+
+//int main() {
+//    int i, sum = 0;
+//    for (i = 0; i < 10001; i++) {
+//        sum = sum + i;
+////        printf("%d \n", sum);
+//    }
+//    printf("1부터 10,000가지의 합 : %d", sum);
+//    return 0;
+//}
+
+//int main() {
+//    int i;
+//    int subject, score;
+//    double sum_score = 0;
+//
+//    printf("몇 개의 과목 점수를 입력받을 것인가요?");
+//    scanf("%d", &subject);
+//
+//    printf("\n 각 과목의 점수를 입력해 주세요 \n");
+//    for (i = 1; i <= subject; i++) {
+//        printf("과목 %d : ", i);
+//        scanf("%d", &score);
+//        sum_score = sum_score + score;
+//    }
+//    printf("전체 과목의 평균은 : %.2f \n", sum_score / subject);
+//    return 0;
+//}
+
+//int main() {
+//    int usranswer;
+//
+//    printf("컴퓨터가 생각한 숫자를 맞추어 보세요: \n");
+//
+//    for (;;) { //for 문에 조건식, 초기식이 없는 경우에는 항상 참(true)
+//        scanf("%d", &usranswer);
+//        if (usranswer == 3) {
+//            printf("맞추셨군요: \n");
+//            break;
+//        } else {
+//            printf("틀렸어요: \n");
+//        }
+//    }
+//    return 0;
+//}
+
+//int main() {
+//    int i;
+//
+//    for (i = 0; i < 100; i++) {
+//        if (i % 5 == 0) continue;
+//
+//        printf("%d ", i);
+//    }
+//    return 0;
+//}
+
+//int main() {
+//    int i, j;
+//
+//    for (i = 2; i < 10; i++) {
+//        for (j = 1; j < 10; j++) {
+//            printf("%d × %d = %d \n", i, j, i*j);
+//        }
+//    }
+//    return 0;
+//}
+
+//int main() {
+//    int i, j;
+//        for (i = 1; i < 10; i++) {
+//            for (j = 1; j < i; j++) {
+//                printf("%d ", j);
+//            }
+//        }
+//    return 0;
+//}
+
+//int main() {
+//    int i = 1, sum = 0;
+//
+//    while (i <= 100) {
+//        sum += i;
+//        i++;
+//    }
+//    printf("1부터 100까지의 합 : %d \n", sum);
+//    return 0;
+//}
+
+//int main() {
+//    int i = 1, sum = 0;
+//
+//    do {
+//        sum += 1;
+//
+//        i++;
+//    } while (i < 1);
+//
+//    printf(" sum : %d \n", sum);
+//    return 0;
+//}
+
+//int main() {
+//    int i, j, n;
+//
+//    printf("원하는 값을 입력하세요 : \n");
+//    scanf("%d", &n);
+//    for (i=0; i <= n; i++) {
+//        if (i == 0) {
+//            for (j = 0; j < 5; j++) {
+//                if (j == 2) {
+//                    printf("*");
+//                } else {
+//                    printf(" ");
+//                }
+//            }
+//        printf("\n");
+//        }
+//        if (i == 1) {
+//            for (j = 0; j < 5; j++) {
+//                if (j == 1 || j == 2 || j == 3) {
+//                    printf("*");
+//                } else {
+//                    printf(" ");
+//                }
+//            }
+//        printf("\n");
+//        }
+//        if (i == 2) {
+//            for (j = 0; j < 5; j++) {
+//                printf("*");
+//            }
+//        printf("\n");
+//        }
+//    }
+//    return 0;
+//}
+
+//int main() {
+//    int i, j, n;
+//
+//    printf("원하는 값을 입력하세요 : \n");
+//    scanf("%d", &n);
+//    for (i=0; i <= n; i++) {
+//        if (i == 2) {
+//            for (j = 0; j < 5; j++) {
+//                if (j == 2) {
+//                    printf("*");
+//                } else {
+//                    printf(" ");
+//                }
+//            }
+//            printf("\n");
+//        }
+//        if (i == 1) {
+//            for (j = 0; j < 5; j++) {
+//                if (j == 1 || j == 2 || j == 3) {
+//                    printf("*");
+//                } else {
+//                    printf(" ");
+//                }
+//            }
+//            printf("\n");
+//        }
+//        if (i == 0) {
+//            for (j = 0; j < 5; j++) {
+//                printf("*");
+//            }
+//            printf("\n");
+//        }
+//    }
+//    return 0;
+//}
+
 int main() {
-    float ave_score;
-    float math, english, science, programming;
+    int input;
 
-    printf("수학, 영어, 과학, 컴퓨터 프로그래밍 점수를 각각 입력해 주세요! ");
-    scanf("%f %f %f %f", &math, &english, &science, &programming);
+    printf("마이펫 업그레이드 \n");
+    printf("무엇을 하실 것인지 입력하세요 \n");
+    printf("1. 밥주기 \n");
+    printf("2. 씻기기 \n");
+    printf("3. 재우기 \n");
 
-    ave_score =
-            (math + english + science + programming) / 4;
-    printf("당신의 평균 점수는 %f입니다. \n", ave_score);
-    if (ave_score >= 90) {
-        printf("당신은 우등생입니다. ");
-    } else if (ave_score >= 30) {
-        printf("조금만 노력하세요!. \n");
-    } else {
-        printf("공부를 발로 합니까? \n");
+    scanf("%d", &input);
+
+    switch (input) {
+        case 1:
+            printf("아이 맛있어! \n");
+            break;
+        case 2:
+            printf("아이 시원해! \n");
+            break;
+        case 3:
+            printf("zzz \n");
+            break;
+        default:
+            printf("무슨 명령인지 못 알아듣겠어. 왈왈 \n");
+            break;
     }
     return 0;
 }
